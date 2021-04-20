8b0cd4b4f3ae3bb7e09d200a3ffb0c7b107f968b
felipeolvr <luizfelipe.lins@gmail.com>
2021-04-20 17:59:39 -0400

    alterado alias controller

--- lib/nl5inmana.ex
+++ lib/nl5inmana.ex
@@ -1,9 +1,4 @@
 defmodule Nl5inmana do
-  @moduledoc """
-  Nl5inmana keeps the contexts that define your domain
-  and business logic.
-
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+  alias Nl5inmana.Restaurants.Create
+  defdelegate create_restaurant(params), to: Create, as: :call
 end
--- lib/nl5inmana_web/controllers/restaurants_controller.ex
+++ lib/nl5inmana_web/controllers/restaurants_controller.ex
@@ -1,13 +1,13 @@
 defmodule Nl5inmanaWeb.RestaurantsController do
   use Nl5inmanaWeb, :controller
   alias Nl5inmana.Restaurant
-  alias Nl5inmana.Restaurants.Create
+
   alias Nl5inmanaWeb.FallbackController
 
   action_fallback FallbackController
 
   def create(conn, params) do
-    with {:ok, %Restaurant{} = restaurant} <- Create.call(params) do
+    with {:ok, %Restaurant{} = restaurant} <- Nl5inmana.create_restaurant(params) do
       conn
       |> put_status(:created)
       |> render("create.json", restaurant: restaurant)
